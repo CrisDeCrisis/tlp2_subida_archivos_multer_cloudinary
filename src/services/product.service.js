d551e3783b1adccbed42650e4902a6a96857012e
CristianD.Gonzalez <cristiand.gonzalez094@gmail.com>
2024-10-16 15:31:23 -0300

    save produtcs in localdatabase

--- src/services/product.service.js
+++ src/services/product.service.js
@@ -1,3 +1,4 @@
+import { products } from '../database/products.js';
 import { uploadFile } from '../utils/cloudinary.utility.js';
 
 export const productsService = {};
@@ -7,12 +8,15 @@ productsService.createProduct = async (product) => {
         const { name, description, price } = product.body;
         const filePath = product.file.path;
         const uploadedImage = await uploadFile(filePath);
-        return {
+        const newProduct = {
+            id: products.length + 1,
             name,
             description,
             price,
             imageUrl: uploadedImage.secure_url
         };
+        products.push(newProduct);
+        return newProduct;
     } catch (error) {
         throw error;
     }
