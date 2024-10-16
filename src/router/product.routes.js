58ab20dbd9984f4b6824e5c0e56a9216a1b20b73
CristianD.Gonzalez <cristiand.gonzalez094@gmail.com>
2024-10-16 15:11:45 -0300

    validations products fields

--- src/router/product.routes.js
+++ src/router/product.routes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { productsCtrl } from '../controllers/product.controller.js';
 import { uploadMiddleware } from '../middlewares/upload.middleware.js';
+import { createProductValidation } from '../validations/product.validation.js';
+import { applyProductValidation } from '../middlewares/productValidation.middleware.js';
 
 
 export const productsRouter = Router();
 
-productsRouter.post('/', uploadMiddleware('productImg'), productsCtrl.createProduct);
+productsRouter.post('/',
+    createProductValidation,
+    applyProductValidation,
+    uploadMiddleware('productImg'),
+    productsCtrl.createProduct);
